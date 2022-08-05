14ec7c7eed88b6e9a0ad4b59ec9e5f962f6f717f
Arcka <valerie.lecoeur75@gmail.com>
2022-08-05 10:41:48 +0200

    initialisation du projet

---
+++ index.js
@@ -0,0 +1,53 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+
+//modif de la taille du canvas (par defaut 300/150)
+const CANVAS_WIDTH = (canvas.width = 600);
+const CANVAS_HEIGHT = (canvas.height = 600);
+//taille des images sur la feuille de sprite
+// largeur de la feuille 6876  / nbr d'image 12 = 573
+const spriteWidth = 575;
+//hauteur de la feuille   5230 /nbr d'image 10 = 523
+const spriteHeight = 523;
+//numero de frame
+let frameX = 0;
+let frameY = 3;
+
+//nombre frame par ligne
+
+//avancer
+let mouvX = 0;
+
+const playerImage = new Image();
+playerImage.src = "images/shadow_dog.png";
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  //ctx.drawImage(image,sourcex,sourcey,sourcew,sourceh, placex,placey,placew,placeh)
+  ctx.drawImage(
+    playerImage,
+    frameX * spriteWidth,
+    frameY * spriteHeight,
+    spriteWidth,
+    spriteHeight,
+    -300 + mouvX,
+    300,
+    CANVAS_WIDTH / 2,
+    CANVAS_HEIGHT / 2
+  );
+  if (mouvX++ > 600) {
+    mouvX = 0;
+  }
+  if (frameX < 8) {
+    mouvX++;
+    frameX++;
+  } else {
+    frameX = 0;
+    mouvX++;
+  }
+
+  requestAnimationFrame(animate);
+}
+
+animate();
